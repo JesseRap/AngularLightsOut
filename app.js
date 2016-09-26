59177334a23c84d108c6852476b7612f5e3e3f30
JesseRap <JRap@jesse-rap.local>
2016-09-26 14:30:59 -0400

    Get # of cells dynamically
    Generate cells using ng-repeat, with # of cells specified by user input
    through radio buttons, also generated with ng-repeat.

--- app.js
+++ app.js
@@ -6,32 +6,51 @@
 
   LightsOutController.$inject = ["$scope"];
   function LightsOutController($scope) {
-    $scope.nCells = 9;
-    var gridWidth = 3;
-    var cellWidth = Math.round(450 / gridWidth);
-    document.styleSheets[1].insertRule(".cell {width: "+cellWidth+"px; height: "+cellWidth+"px;}", this.length-1);
+    $scope.nRows = 3;
+    $scope.nRowsObj = {nRows: 3};
+    $scope.nCells = $scope.nRows**2;
+    var gridWidth = $scope.nRows;
+    console.log(gridWidth);
+    $scope.cellWidth = Math.round(450 / gridWidth);
+    document.styleSheets[1].insertRule(".cell {width: "+$scope.cellWidth+"px; height: "+$scope.cellWidth+"px;}", document.styleSheets[1].length-1);
     $scope.classes = ["lightsOut", "lightsOn"];
     $scope.cellArray = [];
     var cellMatrix = [];
 
-    for (var i=0; i < $scope.nCells; i++) {
-      var cell = {
-        idx: i,
-        classIdx: 1,
-        xVal: Math.floor(i/gridWidth),
-        yVal: i % gridWidth
-      };
-      $scope.cellArray.push(cell);
-    };
+    function makeGrid() {
+      console.log("MAKE GRID", document.styleSheets[1].cssRules[0]);
+      document.styleSheets[1].deleteRule(0);
+      console.log("MAKE GRID", document.styleSheets[1].cssRules[0]);
 
-    for (var i=0; i < gridWidth; i++) {
-      var temp = [];
-      for (var j=0; j < gridWidth; j++) {
-        temp.push($scope.cellArray[(i*gridWidth)+j]);
+      gridWidth = $scope.nRows;
+      $scope.cellWidth = Math.round(450 / gridWidth);
+      document.styleSheets[1].insertRule(".cell {width: "+$scope.cellWidth+"px; height: "+$scope.cellWidth+"px;}", document.styleSheets[1].length-1);
+      console.log("MAKE GRID", document.styleSheets[1].cssRules[0]);
+      console.log(gridWidth, $scope.cellWidth);
+      // document.styleSheets[1].cssRules[0].cssText = ".cell {width: "+$scope.cellWidth+"px; height: "+$scope.cellWidth+"px;}";
+
+      $scope.cellArray = [];
+      for (var i=0; i < $scope.nCells; i++) {
+        var cell = {
+          idx: i,
+          classIdx: 1,
+          xVal: Math.floor(i/gridWidth),
+          yVal: i % gridWidth
+        };
+        $scope.cellArray.push(cell);
       };
-      cellMatrix.push(temp);
+
+      cellMatrix = [];
+      for (var i=0; i < gridWidth; i++) {
+        var temp = [];
+        for (var j=0; j < gridWidth; j++) {
+          temp.push($scope.cellArray[(i*gridWidth)+j]);
+        };
+        cellMatrix.push(temp);
+      };
+      console.log(cellMatrix);
     };
-    console.log(cellMatrix);
+    makeGrid();
 
     var changeCell = function(el) {
       console.log("HELLO ", el);
@@ -42,18 +61,26 @@
     };
 
     $scope.getClass = function(idx){
-      console.log("GETCLASS", $scope.cellArray[idx], $scope.classes[$scope.cellArray[idx].classIdx]);
+      // console.log("GETCLASS", $scope.cellArray[idx], $scope.classes[$scope.cellArray[idx].classIdx]);
       return $scope.classes[$scope.cellArray[idx].classIdx];
     };
 
     $scope.clickCell = function(idx) {
-      console.log("CLICK CELL ", idx);
+      console.log("CLICK CELL ", idx, $scope.nRows, $scope.nRowsObj);
       var el = $scope.cellArray[idx];
       console.log(el);
       if (!$scope.hasWon()) {
         getNeighbors(el).forEach(function(obj) {changeCell(obj)});
       };
     };
+
+    $scope.$watch('nRowsObj.nRows', function() {
+        console.log("nRows " , $scope.nRows, $scope.nRowsObj);
+        $scope.nCells = $scope.nRowsObj.nRows ** 2;
+        $scope.nRows = $scope.nRowsObj.nRows;
+        // $scope.nCells = $scope.nRows**2;
+        makeGrid();
+    });
 
     $scope.reset = function() {
       $scope.cellArray.forEach(function(obj) {obj.classIdx = 1});
@@ -69,17 +96,19 @@
       console.log(result,x,y);
       for (var i = -1; i < 2; i += 2) {
         if (x+i >= 0 && x+i < gridWidth) {
-          console.log("TRYING1 ", x+i, y);
+          // console.log("TRYING1 ", x+i, y);
           result.push(cellMatrix[x+i][y]);
         };
         if (y+i >= 0 && y+i < gridWidth) {
-          console.log("TRYING2 ", x, y+i);
+          // console.log("TRYING2 ", x, y+i);
           result.push(cellMatrix[x][y+i]);
         }
       };
-      console.log("NEIGHBORS: ", result);
+      // console.log("NEIGHBORS: ", result);
       return result;
     };
+
+
 
   };
 
